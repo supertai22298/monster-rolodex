20f23a166ef1903c5205c142c0266da9ac51eb39
Nguyen Van Tai <supertai22298@gmail.com>
2020-07-08 16:55:37 +0700

    Add card components and styles

---
+++ src/components/card/card.components.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './card.styles.css'
+
+export const Card = (props) => {
+  let monster = props.monster
+  let imgSrc = `https://robohash.org/${monster.id}?set=set3&size=180x180`
+
+  return (
+    <div className="card-container">
+      <img src={imgSrc} alt={monster.name} />
+      <h2>{monster.name}</h2>
+      <p>{monster.email}</p>
+    </div>
+  )
+}
