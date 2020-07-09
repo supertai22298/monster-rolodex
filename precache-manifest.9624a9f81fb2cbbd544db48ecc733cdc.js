5d03e9426ac71ff3fa724a19ae279fda94b86926
Nguyen Van Tai <supertai22298@gmail.com>
2020-07-09 15:10:18 +0700

    Updates

---
+++ precache-manifest.9624a9f81fb2cbbd544db48ecc733cdc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bad773570036af247ddca8fb3748f84e",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "6391047f119a79a0ba02",
+    "url": "/monster-rolodex/static/css/main.e6c40473.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6391047f119a79a0ba02",
+    "url": "/monster-rolodex/static/js/main.654c207a.chunk.js"
+  },
+  {
+    "revision": "90506cc279e62bb14608",
+    "url": "/monster-rolodex/static/js/runtime-main.59621b72.js"
+  }
+]);
